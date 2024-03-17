cb0595682787ed4eab74ed7d682797b017abdfde
sachinSingh53 <20it095@jssaten.ac.in>
2024-03-17 16:33:57 +0530

    minor fix in routes

--- src/routes/health.js
+++ src/routes/health.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/gateway-health',(req,res)=>{
-    res.status(200).send('API gateway health is Healthy');
+    res.status(200).send('API gateway is healthy and running');
 })
 
 module.exports = router;
