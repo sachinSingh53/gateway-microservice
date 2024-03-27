6db00d62c479f330cb23a7ba415862ee4b4a2e09
sachinSingh53 <20it095@jssaten.ac.in>
2024-03-27 18:41:42 +0530

    added signin controller and routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { create as createAuth } from '../controllers/auth/signup.js';
+import { create } from '../controllers/auth/signup.js';
+import { read } from '../controllers/auth/signin.js';
 
 const router = express.Router();
 
-router.post('/auth/signup', createAuth);
-
+router.post('/auth/signup', create);
+router.post('/auth/signin', read);
 // router.get('/check',(req,res)=>{
 //     res.send('working fine');
 // })
