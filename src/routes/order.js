36cc544e202fd51a75ccb6ad63bf2b9dfe976b29
sachinSingh53 <20it095@jssaten.ac.in>
2024-06-06 10:13:41 +0530

    minor changes

--- src/routes/order.js
+++ src/routes/order.js
@@ -6,18 +6,18 @@ import{Update} from '../controllers/order/update.js';
 
 const router = express.Router();
 
-this.router.get('/order/notification/:userTo',authMiddleware.verifyUser, Get.prototype.notifications);
-this.router.get('/order/:orderId',authMiddleware.verifyUser, Get.prototype.orderId);
-this.router.get('/order/seller/:sellerId',authMiddleware.verifyUser, Get.prototype.sellerOrders);
-this.router.get('/order/buyer/:buyerId',authMiddleware.verifyUser, Get.prototype.buyerOrders);
-this.router.post('/order',authMiddleware.verifyUser, Create.prototype.order);
-this.router.post('/order/create-payment-intent',authMiddleware.verifyUser, Create.prototype.intent);
-this.router.put('/order/cancel/:orderId',authMiddleware.verifyUser, Update.prototype.cancel);
-this.router.put('/order/extension/:orderId',authMiddleware.verifyUser, Update.prototype.requestExtension);
-this.router.put('/order/deliver-order/:orderId',authMiddleware.verifyUser, Update.prototype.deliverOrder);
-this.router.put('/order/approve-order/:orderId',authMiddleware.verifyUser, Update.prototype.approveOrder);
-this.router.put('/order/gig/:type/:orderId',authMiddleware.verifyUser, Update.prototype.deliveryDate);
-this.router.put('/order/notification/mark-as-read',authMiddleware.verifyUser, Update.prototype.markNotificationAsRead);
+router.get('/order/notification/:userTo',authMiddleware.verifyUser, Get.prototype.notifications);
+router.get('/order/:orderId',authMiddleware.verifyUser, Get.prototype.orderId);
+router.get('/order/seller/:sellerId',authMiddleware.verifyUser, Get.prototype.sellerOrders);
+router.get('/order/buyer/:buyerId',authMiddleware.verifyUser, Get.prototype.buyerOrders);
+router.post('/order',authMiddleware.verifyUser, Create.prototype.order);
+router.post('/order/create-payment-intent',authMiddleware.verifyUser, Create.prototype.intent);
+router.put('/order/cancel/:orderId',authMiddleware.verifyUser, Update.prototype.cancel);
+router.put('/order/extension/:orderId',authMiddleware.verifyUser, Update.prototype.requestExtension);
+router.put('/order/deliver-order/:orderId',authMiddleware.verifyUser, Update.prototype.deliverOrder);
+router.put('/order/approve-order/:orderId',authMiddleware.verifyUser, Update.prototype.approveOrder);
+router.put('/order/gig/:type/:orderId',authMiddleware.verifyUser, Update.prototype.deliveryDate);
+router.put('/order/notification/mark-as-read',authMiddleware.verifyUser, Update.prototype.markNotificationAsRead);
 
 
 
