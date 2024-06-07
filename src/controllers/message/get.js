8b21426b03304ffe854730c20234735508428063
sachinSingh53 <20it095@jssaten.ac.in>
2024-06-07 12:19:46 +0530

    tested and fix the minor bugs in chat-service

--- src/controllers/message/get.js
+++ src/controllers/message/get.js
@@ -6,11 +6,12 @@ import { messageService } from '../../services/api/message-service.js';
 export class Get {
     async conversation(req, res) {
         const response = await messageService.getConversation(req.params.senderUsername, req.params.receiverUsername);
-        res.status(StatusCodes.OK).json({ message: response.data.message, conversations: response.data.conversations });
+        res.status(StatusCodes.OK).json({ message: response.data.message, conversations: response.data.conversation });
     }
 
     async messages(req, res) {
         const response = await messageService.getMessages(req.params.senderUsername, req.params.receiverUsername);
+
         res.status(StatusCodes.OK).json({ message: response.data.message, messages: response.data.messages });
     }
 
@@ -22,6 +23,7 @@ export class Get {
 
     async userMessages(req, res) {
         const { conversationId } = req.params;
+
         const response = await messageService.getUserMessages(conversationId);
         res.status(StatusCodes.OK).json({ message: response.data.message, messages: response.data.messages });
     }
--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -4,12 +4,12 @@ import{Get} from '../controllers/message/get.js'
 import{Create} from '../controllers/message/create.js'
 import{ Update } from '../controllers/message/update.js'
 
-const router = express.Router();
+const  router = express.Router();
 
 router.get('/message/conversation/:senderUsername/:receiverUsername',authMiddleware.verifyUser, Get.prototype.conversation );
 router.get('/message/conversations/:username',authMiddleware.verifyUser, Get.prototype.conversationList);
 router.get('/message/:senderUsername/:receiverUsername',authMiddleware.verifyUser, Get.prototype.messages);
-router.get('/message/conversation/:conversationId',authMiddleware.verifyUser, Get.prototype.userMessages);
+router.get('/message/:conversationId',authMiddleware.verifyUser, Get.prototype.userMessages);
 router.post('/message/',authMiddleware.verifyUser, Create.prototype.message);
 router.put('/message/offer',authMiddleware.verifyUser, Update.prototype.offer);
 router.put('/message/mark-as-read',authMiddleware.verifyUser, Update.prototype.markSingleMessage);
